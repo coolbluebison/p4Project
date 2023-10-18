3343d285d0644b51419ca71c73de173c48253c7e
root <+>
2023-10-18 15:04:29 -0600

    10.18.23 3:04pm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Additional imports
+
+import {BrowserRouter, Route, NavLink, Outler, Routes} from 'react-router-dom'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
